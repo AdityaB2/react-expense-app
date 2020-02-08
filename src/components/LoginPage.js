5059c9eb698bddbfe306cc2723f481b7f613cbc9
AdityaB2 <adityaranjan.ar3@gmail.com>
2020-02-09 00:12:46 +0530

    delete palyground

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,11 +5,12 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Expensify App</h1>
+      <h1 className="box-layout__title">Expenses App</h1>
       <p>It's time to get your expenses under control.</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
+
 );
 
 const mapDispatchToProps = (dispatch) => ({
